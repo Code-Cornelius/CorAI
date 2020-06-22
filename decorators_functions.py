5a5b5b1fc9116633f9942cac129b00fdc03be073
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-06-22 19:31:20 +0100

    Try to make memoization faster.

--- decorators_functions.py
+++ decorators_functions.py
@@ -7,20 +7,19 @@ def Memoization(key_names):
             functools.update_wrapper(self, func)
             self.func = func
             self.dictionary = {}
-            if key_names is not None:
-                self.key_names = key_names
-            else:
-                self.key_names = {(signature(self.func).parameters.values())[0]: 0}  # getting the first parameter
+            self.key_names = key_names
+            # No default behaviour
+            # if key_names is not None:
+            #     self.key_names = key_names
+            # else:
+            #     self.key_names = {(signature(self.func).parameters.values())[0]: 0}  # getting the first parameter
 
         def __call__(self, *args, **kwargs):
             keys = []
-
             # get all the elements that build the key
             for key in self.key_names:
                 keys.append(kwargs[key])
-
-            # pack them into a tuple
-            key = tuple(keys)
+            key = tuple(keys) # pack them into a tuple
 
             if key not in self.dictionary:
                 self.dictionary[key] = self.func(*args, **kwargs)
