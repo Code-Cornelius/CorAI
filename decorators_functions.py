830860f57c1fd8ee019e08ce6126d018b367569c
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-06-25 18:49:02 +0100

    Timer decorator found online.

--- decorators_functions.py
+++ decorators_functions.py
@@ -1,4 +1,5 @@
 import functools
+import time
 from inspect import signature  # know signature of a function
 
 def Memoization(key_names):
@@ -31,3 +32,18 @@ def Memoization(key_names):
             self.dictionary.clear()
 
     return MemoizationClass
+
+
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
