83cfc1031c6b33160a7773a3741c2dc9a5d244ae
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-08-08 16:26:32 +0100

    curry function

--- classical_functions.py
+++ classical_functions.py
@@ -295,4 +295,18 @@ def roundrobin(*iterables):
             pending -= 1
             nexts = itertools.cycle(itertools.islice(nexts, pending))
 
-roundrobin()
+
+# curry is a concept transforming f(x,...,z) into f(x)...(z)
+def curry(f):
+    argc = f.__code__.co_argcount
+    f_args = []
+    f_kwargs = {}
+    def g(*args, **kwargs):
+        nonlocal f_args, f_kwargs
+        f_args += args
+        f_kwargs.update(kwargs)
+        if len(f_args)+len(f_kwargs) == argc:
+            return f(*f_args, **f_kwargs)
+        else:
+            return g
+    return g
