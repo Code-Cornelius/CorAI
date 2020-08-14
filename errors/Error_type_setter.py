18ccdde9184f7e2f9b126af93d8f60c23a244af9
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-08-14 15:26:26 +0100

    new error type for setters.

---
+++ errors/Error_type_setter.py
@@ -0,0 +1,4 @@
+class Error_type_setter(TypeError):
+    def __init__(self, reason = ''):
+        self.message = " ".join(["Argument in setter is not of the good type. ", reason])
+        super().__init__(self.message)
