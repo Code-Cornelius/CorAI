a4043d5f656218991c0217eb736330cc302d2bde
Catea Bianca <cateabianca@gmail.com>
2021-09-15 23:24:04 +0200

    Update setup.py for error

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"error": "priv_lib_error/src"},
-    packages=setuptools.find_packages(where="priv_lib_error/src"),
+    package_dir={"error": "priv_lib_error"},
+    packages=setuptools.find_packages(where="priv_lib_error"),
     python_requires=">=3.6",
 )
