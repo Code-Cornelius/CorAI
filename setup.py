055f5aa3e69f1cda1a513ddae4d79fb5a693903f
Catea Bianca <cateabianca@gmail.com>
2021-09-15 23:03:24 +0200

    Add packing functions

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_libraries",
+    version="0.0.1",
+    author="Niels Cariou-Kotlarek",
+    author_email="cateabianca@gmail.com",
+    description="A collection of python libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Code-Cornelius/python_libraries",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"estimator": "priv_lib_estimator"},
+    packages=setuptools.find_packages(where="priv_lib_estimator"),
+    python_requires=">=3.6",
+)
