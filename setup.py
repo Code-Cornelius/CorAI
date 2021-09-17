2997c01c0cb22c44f42167426cb58cfe43c5d787
BiancaTC <46866370+BiancaTC@users.noreply.github.com>
2021-09-17 21:43:47 +0200

    Packing (#1)
    * Add packing functions
    
    * Update setup.py for error
    
    * Update setup.py for error
    
    * Update structure
    
    * Revert "Update structure"
    
    This reverts commit 876a3e8c
    
    * Update setup file paths
    
    * Fix path for lib error in setup
    
    * Change path to root
    
    * Update setup.py
    
    * Update setup.py
    
    * Update setup.py
    
    * Update setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_libraries",
+    version="0.0.1",
+    author="Niels Cariou-Kotlarek",
+    author_email="cateabianca@gmail.com",
+    description="A collection of python libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Code-Cornelius/python_libraries",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"priv_lib_error": "priv_lib_error",
+                 "priv_lib_estimator": "priv_lib_estimator",
+                 "priv_lib_metaclass": "priv_lib_metaclass",
+                 "priv_lib_ml": "priv_lib_ml",
+                 "priv_lib_plot": "priv_lib_plot",
+                 "priv_lib_util": "priv_lib_util"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
